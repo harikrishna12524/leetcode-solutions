1ede5b6901b033f953460bc266727d7c1b65fe58
Harikrishna <harikrishna12524@gmail.com>
2025-07-21 20:38:30 +0530

    Committing Longest Consecutive Sequence - no set manipulation

--- 128. Longest Consecutive Sequence.py
+++ 128. Longest Consecutive Sequence.py
@@ -3,21 +3,14 @@ class Solution:
         numSet = set(nums)
 
         sol = 0
-        while(len(numSet) > 0):
-            temp = 0
-            curNum = numSet.pop()
-            numSet.add(curNum)
-
-            cSol = self.findCon(curNum, numSet)
-            if(cSol > sol):
-                sol = cSol
+        for n in numSet:
+            if(n-1 not in numSet):
+                cSol = 1
+                while(n+1 in numSet):
+                    cSol += 1
+                    n += 1
+                
+                if(cSol > sol):
+                    sol = cSol
 
         return sol
-
-    def findCon(self, cNum, nSet):
-        if(cNum not in nSet):
-            return 0
-
-        nSet.remove(cNum)
-        sol1 = self.findCon(cNum - 1, nSet) + self.findCon(cNum + 1, nSet) + 1
-        return sol1
