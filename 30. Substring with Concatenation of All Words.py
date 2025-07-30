afde7e8a5792ce43f3699ac3941a8b0786e42c91
Harikrishna <harikrishna12524@gmail.com>
2025-07-30 08:26:36 +0530

    Committing optimap solution for substring with concat

--- 30. Substring with Concatenation of All Words.py
+++ 30. Substring with Concatenation of All Words.py
@@ -10,26 +10,36 @@ class Solution:
         mTL = len(words) * uL
 
         sol = []
-        for i in range(len(s)-mTL + 1):
-
+        for i in range(uL):
             have = 0
-            locWordMap = {}
-            curPos = i
-            while(curPos + uL <= len(s)):
-                substr = s[curPos : curPos + uL]
-                if(substr not in wordMap):
-                    break
+            haveMap = {}
+            strPos = i
+            for j in range(i, len(s), uL):
+                # Add a condition whether length is okay for solution
+                word = s[j: j+uL]
+                if(word not in wordMap):
+                    strPos = j+uL
+                    have = 0
+                    haveMap = {}
+                    continue
                 
-                locWordMap[substr] = locWordMap.get(substr,0) + 1
-                if(locWordMap[substr] == wordMap[substr]):
-                    have += 1
-                elif(locWordMap[substr] > wordMap[substr]):
-                    break
+                haveMap[word] = haveMap.get(word, 0) + 1
+                # print("Here for word :  "+ word + ".Have Map :" + str(haveMap))
+                if(haveMap[word] == wordMap[word]):
+                    have+=1
+                elif(haveMap[word] > wordMap[word]):
+                    while(haveMap[word] > wordMap[word]):
+                        strWord = s[strPos : strPos + uL]
+                        if(haveMap[strWord] == wordMap[strWord]):
+                            have-=1
+                        haveMap[strWord] -= 1
+                        strPos = strPos + uL
 
                 if(have == need):
-                    sol.append(i)
-                    break
-
-                curPos += uL
+                    sol.append(strPos)
+                    strWord = s[strPos : strPos + uL]
+                    have-=1
+                    haveMap[strWord] -= 1
+                    strPos = strPos + uL
         
         return sol
